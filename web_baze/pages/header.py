c0889b415a5b4d07e49729c99a16aef9846a85d2
Alexandra Levina <93614091+Drumlex17@users.noreply.github.com>
2026-01-12 09:36:29 +0300

    Убрала захардкоженный текст, создала атрибуты класса для текстов

--- web_baze/pages/header.py
+++ web_baze/pages/header.py
@@ -4,6 +4,19 @@ from web_baze.data.users import UserData
 
 
 class Header:
+    TAB_TEXT_MAIN = "ГЛАВНАЯ"
+    TAB_TEXT_COMMUNITY = "СООБЩЕСТВА"
+    TAB_TEXT_RATING = "РЕЙТИНГ"
+    TAB_TEXT_NEWS = "НОВОСТИ"
+    TAB_TEXT_ROADMAP = "ДОРОЖНАЯ КАРТА"
+    TAB_TEXT_REGISTRATION = "РЕГИСТРАЦИЯ"
+    TAB_TEXT_REFILL = "ПОПОЛНЕНИЕ"
+    TAB_TEXT_PROMOCODE = "ПРОМОКОД"
+    EXPECTED_TAB_TEXT_UNAUTHORIZED = [TAB_TEXT_MAIN, TAB_TEXT_COMMUNITY, TAB_TEXT_RATING, TAB_TEXT_NEWS,
+                                      TAB_TEXT_ROADMAP, TAB_TEXT_REGISTRATION]
+    EXPECTED_TAB_TEXT_AUTHORIZED = [TAB_TEXT_MAIN, TAB_TEXT_COMMUNITY, TAB_TEXT_RATING, TAB_TEXT_NEWS, TAB_TEXT_ROADMAP,
+                                    TAB_TEXT_REFILL, TAB_TEXT_PROMOCODE]
+
     def __init__(self):
         """Вкладки для неавторизованного пользователя"""
         self.logo_unauthorized = browser.element(".header__logo")
@@ -18,46 +31,38 @@ class Header:
         self.setting_tab = browser.element("a.header__menu-link")
         self.log_out_tab = browser.element(".ui-box--hoverable[style*='color-white-15']")
 
-
     """Шаги для неавторизованного пользователя"""
+
     def click_tabs_unauthorized(self, text):
         self.tabs_header_unauthorized.element_by(have.text(text)).click()
         return self
 
-
     def click_community_tab_unauthorized(self):
-        self.click_tabs_unauthorized('СООБЩЕСТВА')
+        self.click_tabs_unauthorized(self.TAB_TEXT_COMMUNITY)
         return self
-
 
     def click_rating_tab_unauthorized(self):
-        self.click_tabs_unauthorized('РЕЙТИНГ')
+        self.click_tabs_unauthorized(self.TAB_TEXT_RATING)
         return self
-
 
     def click_news_tab_unauthorized(self):
-        self.click_tabs_unauthorized('НОВОСТИ')
+        self.click_tabs_unauthorized(self.TAB_TEXT_NEWS)
         return self
-
 
     def click_roadmap_tab_unauthorized(self):
-        self.click_tabs_unauthorized('ДОРОЖНАЯ КАРТА')
+        self.click_tabs_unauthorized(self.TAB_TEXT_ROADMAP)
         return self
 
-
-    def click_personal_account_tab_unauthorized(self):
-        self.click_tabs_unauthorized('РЕГИСТРАЦИЯ')
+    def click_registration_tab_unauthorized(self):
+        self.click_tabs_unauthorized(self.TAB_TEXT_REGISTRATION)
         return self
-
 
     @allure.step("Проверка отображения элементов хедера неавторизованным пользователем")
     def should_have_menu_items_unauthorized(self):
         self.logo_unauthorized.should(be.visible)
-        expected_items_unauthorized = ['ГЛАВНАЯ', 'СООБЩЕСТВА', 'РЕЙТИНГ', 'НОВОСТИ', 'ДОРОЖНАЯ КАРТА', 'РЕГИСТРАЦИЯ']
-        for item in expected_items_unauthorized:
+        for item in self.EXPECTED_TAB_TEXT_UNAUTHORIZED:
             self.tabs_header_unauthorized.element_by(have.text(item)).should(be.visible)
         return self
-
 
     @allure.step("Прокликивание элементов хедера неавторизованным пользователем")
     def click_all_tabs_header_unauthorized(self):
@@ -66,41 +71,34 @@ class Header:
         self.click_news_tab_unauthorized()
         self.click_news_tab_unauthorized()
         self.click_roadmap_tab_unauthorized()
-        self.click_personal_account_tab_unauthorized()
-
-
+        self.click_registration_tab_unauthorized()
 
     """Шаги для авторизованного пользователя"""
+
     def click_tabs_authorized(self, text):
         self.tabs_header_authorized.element_by(have.text(text)).click()
         return self
 
-
     def click_main_tab_authorized(self):
-        self.click_tabs_authorized('ГЛАВНАЯ')
+        self.click_tabs_authorized(self.TAB_TEXT_MAIN)
         return self
-
 
     def click_refill_tab_authorized(self):
-        self.click_tabs_authorized('ПОПОЛНЕНИЕ')
+        self.click_tabs_authorized(self.TAB_TEXT_REFILL)
         return self
-
 
     def click_promocode_tab_authorized(self):
-        self.click_tabs_authorized('ПРОМОКОД')
+        self.click_tabs_authorized(self.TAB_TEXT_PROMOCODE)
         return self
-
 
     def click_mail_name_tab_authorized(self):
         self.mail_name_tab.click()
         return self
 
-
     def click_notification_tabs_authorized(self):
         self.notification_tab_open.click()
         self.notification_tab_close.click()
         return self
-
 
     def click_setting_tab_authorized(self):
         self.setting_tab.click()
@@ -111,12 +109,10 @@ class Header:
         self.log_out_tab.click()
         return self
 
-
     @allure.step("Проверка отображения элементов хедера авторизованным пользователем")
     def should_have_menu_items_authorized(self, user: UserData):
         self.logo_authorized.should(be.visible)
-        expected_items_authorized = ['ГЛАВНАЯ', 'СООБЩЕСТВА', 'РЕЙТИНГ', 'НОВОСТИ', 'ДОРОЖНАЯ КАРТА', 'ПОПОЛНЕНИЕ', 'ПРОМОКОД']
-        for point in expected_items_authorized:
+        for point in self.EXPECTED_TAB_TEXT_AUTHORIZED:
             self.tabs_header_authorized.element_by(have.text(point)).should(be.visible)
         masked_email = f"{user.mail[0].upper()}*******@{user.mail.split('@')[1].upper()}"
         self.mail_name_tab.should(be.visible).should(have.text(masked_email))
@@ -124,7 +120,6 @@ class Header:
         self.setting_tab.should(be.visible)
         self.log_out_tab.should(be.visible)
         return self
-
 
     @allure.step("Прокликивание элементов хедера авторизованным пользователем")
     def click_all_tabs_header_authorized(self):
